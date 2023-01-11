d13c4d844e2dbbd62e35f9c0081f82cd72a5954d
Armand LEOPOLD <armand.leopold@outlook.com>
2023-01-11 23:05:05 +0100

    update alternatives

--- tutorials/full-installation/ingest-data.sh
+++ tutorials/full-installation/ingest-data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get install -y libmysqlclient-dev python3-dev default-libmysqlclient-dev build-essential 
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 python -m pip install -r tutorials/full-installation/requirements.txt
 python tutorials/full-installation/ingest-data.py
