823eea6cd0e4342412588c789c1c089208de4493
Armand LEOPOLD <armand.leopold@outlook.com>
2023-01-14 11:53:02 +0100

    up

--- tutorials/full-installation/launch-stack.sh
+++ tutorials/full-installation/launch-stack.sh
@@ -3,7 +3,8 @@
 docker-compose -f docker-compose.yml up -d
 
 # get current user to have same user in airflow container workers.
-echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > ~/.env
+echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
+cp .env ~/.env
 
 # pull & run airflow docker container stack
 docker-compose -f docker-compose-airflow.yaml up -d
